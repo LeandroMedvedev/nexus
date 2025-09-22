129416d86aa7323dc37bae3d52bf652a139c666a
LeandroMedvedev <medvedevleandro@outlook.com>
2025-09-22 19:18:37 -0300

    feat(employee): create employee repository interface

---
+++ src/main/java/com/company/nexus/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.company.nexus.repository;
+
+import com.company.nexus.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
